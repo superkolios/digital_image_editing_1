375658c0c773fa7101838d0187cb4cc09bf14abd
Filippos Tolias <filtol001@yahoo.gr>
2024-04-23 15:22:03 +0300

    Fix return type and refactorring

--- global_hist_eq.py
+++ global_hist_eq.py
@@ -1,4 +1,3 @@
-from PIL import Image
 import numpy as np
 
 
@@ -7,8 +6,8 @@ def get_equalization_transform_of_img( img_array: np.ndarray,) -> np.ndarray:
     for i in img_array:
         for j in i:
             histogram[j] += 1
-    num_of_pixeles = img_array.shape[0] * img_array.shape[1]
-    histogram = histogram/ num_of_pixeles
+    num_of_pixeles = img_array.size
+    histogram = histogram / num_of_pixeles
     yk = np.cumsum(histogram)
     y0 = yk[0]
     if (y0 == 1):
@@ -16,7 +15,7 @@ def get_equalization_transform_of_img( img_array: np.ndarray,) -> np.ndarray:
         print('region has only zeros')
         exit()
     yk = np.round((yk - y0)/(1 - y0) * 255)
-    return yk.astype(int)
+    return yk.astype(np.uint8)
 
 
 def perform_global_hist_equalization(img_array: np.ndarray,) -> np.ndarray:
